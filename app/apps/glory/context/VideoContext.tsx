948e943e58fe7448b6b37d95301bfdd1457f76dd
Matthew Sheehan <matthewsheehan@me.com>
2025-02-01 16:53:30 +0000

    okay vincezo fixed

--- app/apps/glory/context/VideoContext.tsx
+++ app/apps/glory/context/VideoContext.tsx
@@ -60,6 +60,131 @@ const CATEGORIES_DATA: Category[] = [
     content: [
       {
         type: "series",
+        id: "glory",
+        title: "The Glory",
+        rating: "97% Google User Review",
+        thumbnail: "/apps/glory/glory_thumbnail.png",
+        description:
+          "A woman puts a carefully crafted revenge plan in motion after suffering traumatic abuse in high school.",
+        maturityRating: "18+",
+        episodeCount: 16,
+        episodes: [
+          {
+            title: "The Glory Episode 1",
+            videoId: "x8qgvfq",
+            description:
+              "Moon Dong-eun becomes a homeroom teacher at a high school to carry out her revenge plot.",
+            duration: "47:18",
+          },
+          {
+            title: "The Glory Episode 2",
+            videoId: "x8qgvpl",
+            description:
+              "Dong-eun's past is revealed as she meticulously plans her vengeance.",
+            duration: "48:22",
+          },
+          {
+            title: "The Glory Episode 3",
+            videoId: "x8qgw2t",
+            description:
+              "Dong-eun's past is revealed as she meticulously plans her vengeance.",
+            duration: "48:22",
+          },
+          {
+            title: "The Glory Episode 4",
+            videoId: "x8qndtz",
+            description:
+              "Dong-eun's past is revealed as she meticulously plans her vengeance.",
+            duration: "48:22",
+          },
+          {
+            title: "The Glory Episode 5",
+            videoId: "x8qgw2t",
+            description:
+              "Dong-eun's past is revealed as she meticulously plans her vengeance.",
+            duration: "48:22",
+          },
+          {
+            title: "The Glory Episode 6",
+            videoId: "x8reoef",
+            description:
+              "Dong-eun's past is revealed as she meticulously plans her vengeance.",
+            duration: "48:22",
+          },
+          {
+            title: "The Glory Episode 7",
+            videoId: "x8reqi7",
+            description:
+              "Dong-eun's past is revealed as she meticulously plans her vengeance.",
+            duration: "48:22",
+          },
+          {
+            title: "The Glory Episode 8",
+            videoId: "x8rerdi",
+            description:
+              "Dong-eun's past is revealed as she meticulously plans her vengeance.",
+            duration: "48:22",
+          },
+          {
+            title: "The Glory Episode 9",
+            videoId: "x8qm9hq",
+            description:
+              "Dong-eun's past is revealed as she meticulously plans her vengeance.",
+            duration: "48:22",
+          },
+          {
+            title: "The Glory Episode 10",
+            videoId: "x8qm9jj",
+            description:
+              "Dong-eun's past is revealed as she meticulously plans her vengeance.",
+            duration: "48:22",
+          },
+          {
+            title: "The Glory Episode 11",
+            videoId: "x8qo5b1",
+            description:
+              "Dong-eun's past is revealed as she meticulously plans her vengeance.",
+            duration: "48:22",
+          },
+          {
+            title: "The Glory Episode 12",
+            videoId: "x8qo5b4",
+            description:
+              "Dong-eun's past is revealed as she meticulously plans her vengeance.",
+            duration: "48:22",
+          },
+          {
+            title: "The Glory Episode 13",
+            videoId: "x8qo5b3",
+            description:
+              "Dong-eun's past is revealed as she meticulously plans her vengeance.",
+            duration: "48:22",
+          },
+          {
+            title: "The Glory Episode 14",
+            videoId: "x8qo5b5",
+            description:
+              "Dong-eun's past is revealed as she meticulously plans her vengeance.",
+            duration: "48:22",
+          },
+          {
+            title: "The Glory Episode 15",
+            videoId: "x8qo5b2",
+            description:
+              "Dong-eun's past is revealed as she meticulously plans her vengeance.",
+            duration: "48:22",
+          },
+          {
+            title: "The Glory Episode 16",
+            videoId: "x8qo5b6",
+            description:
+              "Dong-eun's past is revealed as she meticulously plans her vengeance.",
+            duration: "48:22",
+          },
+        ],
+      },
+      {
+        type: "series",
         id: "vincezo",
         title: "Vincezo",
         rating: "96% Google User Review",
---
+++ app/apps/glory/context/ff.tsx
@@ -0,0 +1,151 @@
+{
+        type: "series",
+        id: "vincezo",
+        title: "Vincezo",
+        rating: "96% Google User Review",
+        thumbnail: "/apps/glory/vincezo_thumbnail.png",
+        description:
+          "A woman puts a carefully crafted revenge plan in motion after suffering traumatic abuse in high school.",
+        maturityRating: "18+",
+        episodeCount: 16,
+        episodes: [
+          {
+            title: "Vincezo Episode 1",
+            videoId: "x8qgvfq",
+            description:
+              "Moon Dong-eun becomes a homeroom teacher at a high school to carry out her revenge plot.",
+            duration: "47:18",
+          },
+          {
+            title: "Vincezo Episode 2",
+            videoId: "x8qgvpl",
+            description:
+              "Dong-eun's past is revealed as she meticulously plans her vengeance.",
+            duration: "48:22",
+          },
+          {
+            title: "Vincezo Episode 3",
+            videoId: "x8qgw2t",
+            description:
+              "Dong-eun's past is revealed as she meticulously plans her vengeance.",
+            duration: "48:22",
+          },
+          {
+            title: "Vincezo Episode 4",
+            videoId: "x8qndtz",
+            description:
+              "Dong-eun's past is revealed as she meticulously plans her vengeance.",
+            duration: "48:22",
+          },
+          {
+            title: "Vincezo Episode 5",
+            videoId: "x8qgw2t",
+            description:
+              "Dong-eun's past is revealed as she meticulously plans her vengeance.",
+            duration: "48:22",
+          },
+          {
+            title: "Vincezo Episode 6",
+            videoId: "x8reoef",
+            description:
+              "Dong-eun's past is revealed as she meticulously plans her vengeance.",
+            duration: "48:22",
+          },
+          {
+            title: "Vincezo Episode 7",
+            videoId: "x8reqi7",
+            description:
+              "Dong-eun's past is revealed as she meticulously plans her vengeance.",
+            duration: "48:22",
+          },
+          {
+            title: "Vincezo Episode 8",
+            videoId: "x8rerdi",
+            description:
+              "Dong-eun's past is revealed as she meticulously plans her vengeance.",
+            duration: "48:22",
+          },
+          {
+            title: "Vincezo Episode 9",
+            videoId: "x8qm9hq",
+            description:
+              "Dong-eun's past is revealed as she meticulously plans her vengeance.",
+            duration: "48:22",
+          },
+          {
+            title: "Vincezo Episode 10",
+            videoId: "x8qm9jj",
+            description:
+              "Dong-eun's past is revealed as she meticulously plans her vengeance.",
+            duration: "48:22",
+          },
+          {
+            title: "Vincezo Episode 11",
+            videoId: "x8qo5b1",
+            description:
+              "Dong-eun's past is revealed as she meticulously plans her vengeance.",
+            duration: "48:22",
+          },
+          {
+            title: "Vincezo Episode 12",
+            videoId: "x8qo5b4",
+            description:
+              "Dong-eun's past is revealed as she meticulously plans her vengeance.",
+            duration: "48:22",
+          },
+          {
+            title: "Vincezo Episode 13",
+            videoId: "x8qo5b3",
+            description:
+              "Dong-eun's past is revealed as she meticulously plans her vengeance.",
+            duration: "48:22",
+          },
+          {
+            title: "Vincezo Episode 14",
+            videoId: "x8qo5b5",
+            description:
+              "Dong-eun's past is revealed as she meticulously plans her vengeance.",
+            duration: "48:22",
+          },
+          {
+            title: "Vincezo Episode 15",
+            videoId: "x8qo5b2",
+            description:
+              "Dong-eun's past is revealed as she meticulously plans her vengeance.",
+            duration: "48:22",
+          },
+          {
+            title: "Vincezo Episode 16",
+            videoId: "x8qo5b6",
+            description:
+              "Dong-eun's past is revealed as she meticulously plans her vengeance.",
+            duration: "48:22",
+          },
+          {
+            title: "Vincezo Episode 17",
+            videoId: "x8qo5b6",
+            description:
+              "Dong-eun's past is revealed as she meticulously plans her vengeance.",
+            duration: "48:22",
+          },
+          {
+            title: "Vincezo Episode 18",
+            videoId: "x8qo5b6",
+            description:
+              "Dong-eun's past is revealed as she meticulously plans her vengeance.",
+            duration: "48:22",
+          },
+          {
+            title: "Vincezo Episode 19",
+            videoId: "x8qo5b6",
+            description:
+              "Dong-eun's past is revealed as she meticulously plans her vengeance.",
+            duration: "48:22",
+          },
+          {
+            title: "Vincezo Episode 20",
+            videoId: "x8qo5b6",
+            description:
+              "Dong-eun's past is revealed as she meticulously plans her vengeance.",
+            duration: "48:22",
+          },
